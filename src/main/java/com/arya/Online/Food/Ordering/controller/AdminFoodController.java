6ea629b6bbebaf14f5bbc1a11309e99787c97545
arya-chavan <chavanarya00@gmail.com>
2024-04-18 13:17:46 +0530

    food-api

---
+++ src/main/java/com/arya/Online/Food/Ordering/controller/AdminFoodController.java
@@ -0,0 +1,53 @@
+package com.arya.Online.Food.Ordering.controller;
+
+import com.arya.Online.Food.Ordering.model.Food;
+import com.arya.Online.Food.Ordering.model.Restaurant;
+import com.arya.Online.Food.Ordering.model.User;
+import com.arya.Online.Food.Ordering.request.CreateFoodRequest;
+import com.arya.Online.Food.Ordering.response.MessageResponse;
+import com.arya.Online.Food.Ordering.service.FoodService;
+import com.arya.Online.Food.Ordering.service.RestaurantService;
+import com.arya.Online.Food.Ordering.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/food")
+public class AdminFoodController {
+    @Autowired
+    private FoodService foodService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RestaurantService restaurantService;
+
+
+    @PostMapping
+    public ResponseEntity<Food> createFood(@RequestBody CreateFoodRequest req,
+                                           @RequestHeader("Authorization")String jwt) throws Exception {
+        User user =userService.findUserByJwtToken(jwt);
+        Restaurant restaurant=restaurantService.findRestaurantById(req.getRestaurantId());
+        Food food= foodService.createFood(req,req.getCategory(),restaurant);
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponse>deleteFood(@PathVariable Long id,
+                                                     @RequestHeader("Authorization")String jwt) throws Exception {
+        User user =userService.findUserByJwtToken(jwt);
+        foodService.deleteFood(id);
+        MessageResponse res=new MessageResponse();
+        res.setMessage("Successfully deleted food");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Food> updateFoodAvailability(@PathVariable Long id,
+                                                       @RequestHeader("Authorization")String jwt)throws Exception{
+        User user =userService.findUserByJwtToken(jwt);
+        Food food=foodService.updateAvailabilityStatus(id);
+        return new ResponseEntity<>(food,HttpStatus.OK);
+    }
+}
--- src/main/java/com/arya/Online/Food/Ordering/repository/FoodRepository.java
+++ src/main/java/com/arya/Online/Food/Ordering/repository/FoodRepository.java
@@ -2,9 +2,14 @@ package com.arya.Online.Food.Ordering.repository;
 
 import com.arya.Online.Food.Ordering.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FoodRepository extends JpaRepository<Food,Long> {
-    // List<Food>;
+    List<Food> findByRestaurantId(Long restaurantId);
+    @Query("SELECT f FROM Food f WHERE f.name LIKE %:Keyword% OR f.foodCategory.name LIKE %:Keyword%")
+    List<Food>searchFood(@Param("Keyword")String Keyword);
+
 }
